fdeb145bd5a56fcec5d982dbbb7b51437b49d4c4
besumero628 <besumero628@gmail.com>
2022-09-25 20:46:19 +0900

    子コンポーネントから親コンポーネントへ

--- vite_app/src/App.vue
+++ vite_app/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <HelloWorld v-bind:title="message"/>
-    <hr>
-    <button class="btn btn-primary" v-on:click="doAction">change title</button>
+    <HelloWorld v-bind:title="message" v-on:result-event="appAction" />
+    <hr />
+    <p>{{result}}</p>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
   },
   data() {
     return {
-      message:'HELLO',
-    }
+      message: "HELLO",
+      result: 'no event'
+    };
   },
   methods: {
-    doAction() {
-      var input = prompt("new title:")
-      this.message = input
+    appAction(message) {
+      this.result = '(*** you send:"' + message + '".***)'
     }
-  }
-}
+  },
+};
 </script>
