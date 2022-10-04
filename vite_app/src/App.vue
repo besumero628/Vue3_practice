2f27eaeea9aae18c585e27dd530031e368438b39
besumero628 <besumero628@gmail.com>
2022-10-04 23:03:50 +0900

    app.vue更新

--- vite_app/src/App.vue
+++ vite_app/src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  
+  <div id="app">
+    <HelloWorld />
+  </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue';
 export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
