7c75f9a7d57b5ffad35dcf44777d7dfe0793c8ba
besumero628 <besumero628@gmail.com>
2022-10-04 23:54:14 +0900

    dataの埋め込み

--- vite_app/src/components/hello.jsx
+++ vite_app/src/components/hello.jsx
@@ -4,11 +4,17 @@ export default {
     title: String,
     message: String,
   },
+  data() {
+    return {
+      cls_title: 'text-danger h1',
+      cls_msg: 'text-primary h5'
+    }
+  },
   render(h) {
     return (
       <div class="alert alert-primary">
-        <h2>{this.title}</h2>
-        <p>{this.message}</p>
+        <h2 class={this.cls_title}>{this.title}</h2>
+        <p class={this.cls_msg}>{this.message}</p>
       </div>
     )
   }
