bdc3f76f37585aaa70a05436a58df914144d695b
besumero628 <besumero628@gmail.com>
2022-10-04 23:09:13 +0900

    HelloWorldの更新

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="alert alert-primary">
-    <h1>{{title}}</h1>
-    <p ref="msg">{{message}}</p>
-    <button class="btn btn-primary" v-on:click="DoAction">Click</button>
+  <div class="alert alert-info">
+    <h2>{{ title }}</h2>
+    <p>{{ message }}</p>
   </div>
 </template>
 
@@ -13,15 +12,6 @@ export default {
     return {
       title: 'HelloWorld',
       message: 'This is sample message.',
-    }
-  },
-  mounted() {
-    this.counter = 0
-  },
-  methods: {
-    DoAction() {
-      this.counter++
-      this.$refs.msg.innerHTML += '<h6>counted: ' + this.counter + '</h6>'
     }
   }
 }
