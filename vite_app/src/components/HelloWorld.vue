91bf07ffd9459c46ec83c36274c65c4b3ddbf7f1
besumero628 <besumero628@gmail.com>
2022-10-06 10:02:46 +0900

    イベントの追加

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -5,9 +5,13 @@
     <button class="btn btn-primary m-3" v-on:click="doAction">
       {{btn}}
     </button>
-    <transition name="transit">
-      <p v-if="flg" class="alert alert-light h5">
-        Transhition context!
+    <transition name="transit"
+      v-on:before-enter="startAction"
+      v-on:before-leave="startAction"
+      v-on:after-enter="endAction"
+      v-on:after-leave="endAction">
+      <p v-if="flg" class="alert p-3 h5 trans">
+        Transition context!
       </p>
     </transition>
   </div>
@@ -21,18 +25,69 @@ export default {
       title: 'Trans&Anim',
       message: 'This is Transition sample!',
       flg: true,
-      btn: 'Show/Hide'
+      btn: 'Hide'
     }
   },
   methods: {
     doAction() {
       this.flg = !this.flg
+    },
+    startAction: function () {
+      if (this.flg) {
+        this.message = '現れます・・・'
+      } else {
+        this.message = '消えます・・・'
+      }
+      },
+      endAction: function () {
+        if (this.flg) {
+          this.btn = 'Hide'
+          this.message = '現れました。'
+        } else {
+          this.btn = 'Show'
+          this.message = '消えました。'
+        }
     }
   }
 };
 </script>
 
 <style>
+.trans {
+  background-color: black;
+  color: white;
+  padding: 10px;
+  font-size: 20pt;
+}
+
+.transit-enter-active {
+  transition: opacity 0.5s;
+}
+
+.transit-leave-active {
+  transition: opacity 5.0s;
+}
+
+.transit-enter {
+  opacity: 0;
+}
+
+.transit-enter-from {
+  opacity: 0;
+}
+
+.transit-enter-to {
+  opacity: 1.0;
+}
+
+.transit-leave {
+  opacity: 1.0;
+}
+
+.transit-leave-to {
+  opacity: 0;
+}
+
 pre {
   font-size: 16pt;
   line-height: 1.25;
