147761d4390d86a1bd83d4c1216245141406173f
besumero628 <besumero628@gmail.com>
2022-10-06 12:54:56 +0900

    mutation引数

--- vite_app/src/components/HelloWorld.vue
+++ vite_app/src/components/HelloWorld.vue
@@ -3,12 +3,13 @@
     <h1>{{ data.title }}</h1>
     <p class="mt-3 h5">{{ $store.state.message }}</p>
     <hr />
-    <div
-      class="btn btn-secoundary"
-      @click="$store.commit('count')"
-      @click.ctrl="$store.commit('reset')"
+    <div class="alert alert-dark"
+      @click.exact="$store.commit('count', 1)"
+      @click.shift="$store.commit('count', 2)"
+      @click.ctrl="$store.commit('count', 3)"
     >
-    <a class="h5">
+    <a class="h5"
+    @click.stop="$store.commit('reset')">
       clicked: {{$store.state.counter}}
     </a>
     </div>
